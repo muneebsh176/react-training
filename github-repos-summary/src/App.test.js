1a2da3b2f883309151fe3b242778a1a632604c1c
Muneeb Shahid <muneebsh@atrihub.io>
2021-07-06 19:28:55 -0700

    test(NavBar and App): Unit test
    Unit test added for successful authentication.
    
    [ICTRAINING-766]

--- github-repos-summary/src/App.test.js
+++ github-repos-summary/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App'
 import { render, fireEvent, waitFor } from '@testing-library/react'
 import "@testing-library/jest-dom/extend-expect"
 
-test('Authentication error is rendered', async () => {
+test('authentication error is rendered', async () => {
     const { getByTestId } = render(<App />)
 
     const username = getByTestId('username-field')
@@ -16,5 +16,21 @@ test('Authentication error is rendered', async () => {
     fireEvent.submit(loginBtnEl)
 
     const alertEl = await waitFor(() => getByTestId('auth-error'))
+    expect(alertEl).toBeDefined()
+})
+
+test('render navigation bar on successfull authentication', async () => {
+    const { getByTestId, getByText } = render(<App />)
+
+    const username = getByTestId('username-field')
+    const password = getByTestId('password-field')
+
+    fireEvent.change(username, { target: { value: "muneeb706" } })
+    fireEvent.change(password, { target: { value: "admin123" } })
+
+    const loginBtnEl = getByTestId("login-btn")
+    fireEvent.submit(loginBtnEl)
+
+    const alertEl = await waitFor(() => getByText('Commits'))
     expect(alertEl).toBeDefined()
 })
